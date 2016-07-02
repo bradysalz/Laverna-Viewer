e401a7ec373b74964b76c52443171dceee27e7ca
Brady Salz <brady.salz@gmail.com>
2016-07-02 15:05:01 -0400

    init basic classes for parsing

---
+++ parser/Notebook.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+class Notebook():
+	"""A Notebook represents a Laverna Notebook. It can hold both Notes and Notebooks. Notes are accessed through the Notes attribute. Notebooks are accessed through either the children or parent attribute, which points to the respective Notebook in the tree."""
+
+	def __init__(self):
+
+		pass
+
+
+	def add_note(self, Note):
+		pass
+
+	def get_notes(self):
+		pass
+
+	def get_parent(self):
+	 	pass 
+
+	def set_parent(self, Notebook):
+		pass
+
+ 	def get_child(self):
+ 		pass
+
+ 	def add_child(self, child):
+ 		pass
+
+	iem 
+
