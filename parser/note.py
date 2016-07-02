d4e8fed629ad04a864927f8dd80396b70f4b483a
Brady Salz <brady.salz@gmail.com>
2016-07-02 15:32:31 -0400

    proper naming scheme for modules

---
+++ parser/note.py
@@ -0,0 +1,38 @@
+import json
+import datetime
+
+
+class Note():
+    """The Note Class is the core class. It represents a single Laverna Note file. While it primarily just reads in the JSON file to a class, we also can change the content format (using pandoc)."""
+
+    def __init__(self, file=None):
+        # initialize the important keys used elsewhere
+        self.file_name = ''
+        self.content = ''
+        self.tags = ''
+        self.title = ''
+        self.notebookId = ''
+        self.content_format = ''
+        self.created = datetime.datetime.now()
+        self.updated = datetime.datetime.now()
+
+        if file is not None:
+            self.__add_attrs(file)
+
+    def __load_note(self, file):
+        """Load file to JSON"""
+        with open(file, 'r') as file_hdr:
+            note_json = json.load(file_hdr)
+
+        return note_json
+
+    def __add_attrs(self, file):
+        """Adds all the JSON keys as Note attributes"""
+        note_json = self.__load_note(file)
+        for key in note_json:
+            setattr(self, key, note_json[key])
+
+        self.content_format = 'markdown'
+
+    def __repr__(self):
+        return "<Note: {0}>".format(self.title)
---
+++ parser/notebook.py
@@ -0,0 +1,27 @@
+class Notebook():
+    """A Notebook represents a Laverna Notebook. It can hold both Notes and Notebooks. Notes are accessed through the Notes attribute. Notebooks are accessed through either the children or parent attribute, which points to the respective Notebook in the tree."""
+
+    def __init__(self):
+        pass
+
+    def add_note(self, Note):
+        pass
+
+    def get_notes(self):
+        pass
+
+    def get_parent(self):
+        pass
+
+    def set_parent(self, Notebook):
+        pass
+
+    def get_child(self):
+        pass
+
+    def add_child(self, child):
+        pass
+
+    def change_all_child_format(self, new_format):
+        """Set the content format of all child Notes. Applies recursively (to child Notebooks) as well."""
+        pass
